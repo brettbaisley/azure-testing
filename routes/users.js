3048f4d1a263f4dbe5d31090a5fdf4d72c105bc8
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2022-11-20 16:59:24 -0500

    commit two

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource for unknown user');
+});
+
+router.get("/:user", function (req, res) {
+  res.render('user', { user: req.params.user });
 });
 
 module.exports = router;
