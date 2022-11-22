8a6f608414ddbcb06a3b2999da5d216c2f4bff2c
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2022-11-21 19:46:28 -0500

    added cities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
+cities = [{
+  "id": 1,
+  "city": "Waterbury",
+  "state": "Connecticut",
+  "country": "United States"
+}, {
+  "id": 2,
+  "city": "Albany",
+  "state": "New York",
+  "country": "United States"
+}, {
+  "id": 3,
+  "city": "Utica",
+  "state": "New York",
+  "country": "United States"
+}, {
+  "id": 4,
+  "city": "Austin",
+  "state": "Texas",
+  "country": "United States"
+}, {
+  "id": 5,
+  "city": "San Antonio",
+  "state": "Texas",
+  "country": "United States"
+}, {
+  "id": 6,
+  "city": "Memphis",
+  "state": "Tennessee",
+  "country": "United States"
+}, {
+  "id": 7,
+  "city": "Fresno",
+  "state": "California",
+  "country": "United States"
+}, {
+  "id": 8,
+  "city": "Dallas",
+  "state": "Texas",
+  "country": "United States"
+}, {
+  "id": 9,
+  "city": "Boise",
+  "state": "Idaho",
+  "country": "United States"
+}, {
+  "id": 10,
+  "city": "Maple Plain",
+  "state": "Minnesota",
+  "country": "United States"
+}]
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Express', cities: cities });
 });
 
 module.exports = router;
